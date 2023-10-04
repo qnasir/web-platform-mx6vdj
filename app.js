c95f035060f9824c061d0c6a3e82063583c242d2
qnasir <qnasir575@gmail.com>
2023-10-04 09:12:34 +0530

    final

---
+++ app.js
@@ -0,0 +1,122 @@
+var sehwag=document.getElementById("sehwag")
+var sachin=document.getElementById("sachin")
+var gambhir=document.getElementById("gambhir")
+var virat=document.getElementById("virat")
+var dhoni=document.getElementById("dhoni")
+var yuvraj=document.getElementById("yuvraj")
+var suresh=document.getElementById("suresh")
+var harbajan=document.getElementById("harbajan")
+var zaheer=document.getElementById("zaheer")
+var munaf=document.getElementById("munaf")
+var sreesanth=document.getElementById("sreesanth")
+var seh=document.getElementById("seh")
+var sac=document.getElementById("sac")
+var gam=document.getElementById("gam")
+var vir=document.getElementById("vir")
+var dho=document.getElementById("dho")
+var yuv=document.getElementById("yuv")
+var sur=document.getElementById("sur")
+var har=document.getElementById("har")
+var zah=document.getElementById("zah")
+var muna=document.getElementById("muna")
+var sre=document.getElementById("sre")
+
+function showimage() {
+    seh.style.display='block';
+}
+function hideimage() {
+    seh.style.display='none';
+}
+sehwag.addEventListener('click',showimage)
+document.body.addEventListener('mousedown',hideimage)
+
+function showimage1() {
+    sac.style.display='block';
+}
+function hideimage1() {
+    sac.style.display='none';
+}
+sachin.addEventListener('click',showimage1)
+document.body.addEventListener('mousedown',hideimage1)
+
+function showimage2() {
+    gam.style.display='block';
+}
+function hideimage2() {
+    gam.style.display='none';
+}
+gambhir.addEventListener('click',showimage2)
+document.body.addEventListener('mousedown',hideimage2)
+
+function showimage3() {
+    vir.style.display='block';
+}
+function hideimage3() {
+    vir.style.display='none';
+}
+virat.addEventListener('click',showimage3)
+document.body.addEventListener('mousedown',hideimage3)
+
+function showimage4() {
+    dho.style.display='block';
+}
+function hideimage4() {
+    dho.style.display='none';
+}
+dhoni.addEventListener('click',showimage4)
+document.body.addEventListener('mousedown',hideimage4)
+
+function showimage5() {
+    yuv.style.display='block';
+}
+function hideimage5() {
+    yuv.style.display='none';
+}
+yuvraj.addEventListener('click',showimage5)
+document.body.addEventListener('mousedown',hideimage5)
+
+function showimage6() {
+    sur.style.display='block';
+}
+function hideimage6() {
+    sur.style.display='none';
+}
+suresh.addEventListener('click',showimage6)
+document.body.addEventListener('mousedown',hideimage6)
+
+function showimage7() {
+    har.style.display='block';
+}
+function hideimage7() {
+    har.style.display='none';
+}
+harbajan.addEventListener('click',showimage7)
+document.body.addEventListener('mousedown',hideimage7)
+
+function showimage8() {
+    zah.style.display='block';
+}
+function hideimage8() {
+    zah.style.display='none';
+}
+zaheer.addEventListener('click',showimage8)
+document.body.addEventListener('mousedown',hideimage8)
+
+function showimage9() {
+    mun.style.display='block';
+}
+function hideimage9() {
+    mun.style.display='none';
+}
+munaf.addEventListener('click',showimage9)
+document.body.addEventListener('mousedown',hideimage9)
+
+function showimage0() {
+    sre.style.display='block';
+}
+function hideimage0() {
+    sre.style.display='none';
+}
+sreesanth.addEventListener('click',showimage0)
+document.body.addEventListener('mousedown',hideimage0)
+
